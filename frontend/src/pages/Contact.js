b9486a00c69c1bd9d86b688ef4a23ee757d7d94c
Joseph Encarnacion <josephencarnacion62@gmail.com>
2025-04-19 22:20:52 +0800

    i mean is day 4

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -52,7 +52,7 @@ const Contact = () => {
           className="contact-header"
         >
           <h2>Let's work together</h2>
-          <p>I'm always open to discussing new projects, creative ideas or opportunities to be part of your visions.</p>
+          <p>I'm always open to discussing new projects, creative ideas or opportunities to be part of your vision.</p>
         </motion.div>
 
         <div className="contact-content">
